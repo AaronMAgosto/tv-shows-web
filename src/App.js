60a795c878ca9d2d75e38fa5fc06a2ba1b86a738
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-04-27 17:35:25 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home.jsx";
+import AddShow from "./components/AddShow.jsx"
+import Login from "./components/Login.jsx"
+import SignUp from "./components/SignUp.jsx"
+import './styles/App.css'
 
 function App() {
+  const [ shows, setShows] = useState()
+  const [user ,setUser] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home shows={shows} setShows={setShows} />}  />
+        <Route path='/signup' element={<SignUp user={user }setUser={setUser} />}  />
+        <Route path='/addshow' element={<AddShow setShows={setShows} />} />
+        <Route path='/login' element={<Login user={user} setUser={setUser} />} />
+      </Routes>
+      </BrowserRouter>
+      </>
   );
 }
 
